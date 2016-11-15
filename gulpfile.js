777fdcb32002785e82bdaa44aa3cc55cac5ce9dc
Hugues Stefanski <hugues.stefanski@gmail.com>
2016-11-15 10:38:00 +0100

    Firebase configuration Gulp build

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var templatecache = require('gulp-angular-templatecache'); 
+
+let appFiles = ['src/app.js',
+    'src/**/*.controller.js',
+    'src/**/*.directive.js',
+    'src/**/*.service.js'];
+
+gulp.task('scripts', function () {
+    return gulp.src(appFiles)
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('./public/'));
+});
+
+gulp.task('templates', function () {
+    gulp.src(['src/**/*.html', '!src/index.html'])
+        .pipe(templatecache('templates.js',{module:'app'}))
+        .pipe(gulp.dest('./public'));
+});
